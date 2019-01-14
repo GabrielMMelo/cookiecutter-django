0495df52d8cfbbbf6035b8fe854b450a20f732e8
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-01-14 00:03:05 -0200

    Update static path

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/base.py
@@ -150,7 +150,7 @@ STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
+    str(APPS_DIR.path('{{ cookiecutter.project_slug }}/static')),
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
